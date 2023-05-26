d22eb015b0eb8d99d1e0541a0a95e614b2f65633
sudang <tidh1640@naver.com>
2023-05-26 12:48:56 +0900

    Feat✨: 5월 25일 성능개선

--- src/main/java/DataStructure/No12891.java
+++ src/main/java/DataStructure/No12891.java
@@ -1,8 +1,6 @@
 package DataStructure;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class No12891 {
@@ -16,28 +14,26 @@ public class No12891 {
         int s = Integer.parseInt(st.nextToken());
         int p = Integer.parseInt(st.nextToken());
 
-        String[] dna = br.readLine().split("");
+        char[] dna = br.readLine().toCharArray();
 
-        Map<String, Integer> minCnt = new HashMap<>();
         st = new StringTokenizer(br.readLine());
-        minCnt.put("A", Integer.valueOf(st.nextToken()));
-        minCnt.put("C", Integer.valueOf(st.nextToken()));
-        minCnt.put("G", Integer.valueOf(st.nextToken()));
-        minCnt.put("T", Integer.valueOf(st.nextToken()));
+        int a = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int g = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
 
         int cnt = 0, index = 0, pCnt = 0;
         int[] tempCnt = new int[DNA_CHAR_COUNT];
         while (index < s) {
             if (pCnt == p) {
-                deleteHead(p, dna, index, tempCnt);
+                deleteHead(dna[index - p], tempCnt);
             } else {
                 pCnt++;
             }
 
-            addTail(dna, index, tempCnt);
+            addTail(dna[index], tempCnt);
             if (p == pCnt) {
-                if (minCnt.get("A") <= tempCnt[0] && minCnt.get("C") <= tempCnt[1] &&
-                        minCnt.get("G") <= tempCnt[2] && minCnt.get("T") <= tempCnt[3]) {
+                if (a <= tempCnt[0] && c <= tempCnt[1] && g <= tempCnt[2] && t <= tempCnt[3]) {
                     cnt++;
                 }
             }
@@ -49,35 +45,35 @@ public class No12891 {
         br.close();
     }
 
-    private static void addTail(String[] dna, int index, int[] tempCnt) {
-        switch (dna[index]) {
-            case "A":
+    private static void addTail(char ch, int[] tempCnt) {
+        switch (ch) {
+            case 'A':
                 tempCnt[0]++;
                 break;
-            case "C":
+            case 'C':
                 tempCnt[1]++;
                 break;
-            case "G":
+            case 'G':
                 tempCnt[2]++;
                 break;
-            case "T":
+            case 'T':
                 tempCnt[3]++;
                 break;
         }
     }
 
-    private static void deleteHead(int p, String[] dna, int index, int[] tempCnt) {
-        switch (dna[index - p]) {
-            case "A":
+    private static void deleteHead(char ch, int[] tempCnt) {
+        switch (ch) {
+            case 'A':
                 tempCnt[0]--;
                 break;
-            case "C":
+            case 'C':
                 tempCnt[1]--;
                 break;
-            case "G":
+            case 'G':
                 tempCnt[2]--;
                 break;
-            case "T":
+            case 'T':
                 tempCnt[3]--;
                 break;
         }
